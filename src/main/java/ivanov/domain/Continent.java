c1ea19dbf23dbcebf1e5cf3591c543954cb768b9
iliya <iliya00817@gmail.com>
2023-01-08 19:46:15 +0300

    Initial commit

---
+++ src/main/java/ivanov/domain/Continent.java
@@ -0,0 +1,11 @@
+package ivanov.domain;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
---
+++ src/main/java/ivanov/redis/Language.java
@@ -0,0 +1,16 @@
+package ivanov.redis;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+public class Language {
+    private String language;
+    private Boolean isOfficial;
+    private BigDecimal percentage;
+
+
+}
